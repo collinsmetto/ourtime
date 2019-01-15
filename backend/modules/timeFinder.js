5c93aa48ddfd62ac02953b413feecec56ef2cc14
EmmanuelTef123 <eteferi@princeton.edu>
2019-01-15 01:12:14 -0500

    m

--- backend/modules/timeFinder.js
+++ backend/modules/timeFinder.js
@@ -48,10 +48,6 @@ user = {"allUserEvents": [
 
 ]};
 
-<<<<<<< HEAD
-function subtractRanges(longRanges, shortRanges)
-  {
-=======
 function subtractRanges(longRanges, user)
   {
     shortRanges = []
@@ -61,7 +57,6 @@ function subtractRanges(longRanges, user)
         e = moment.range(user.allUserEvents[i].start, user.allUserEvents[i].end); 
         shortRanges.push(e);
     }
->>>>>>> ede40893633336c6ba3d10ef494afb0ab4f9ae63
     // Always return an array
     if(shortRanges.length === 0)
       return longRanges.hasOwnProperty("length")
@@ -106,133 +101,11 @@ function subtractRanges(longRanges, user)
 
 
 
-<<<<<<< HEAD
-/*
-duration should be sent in as minutes.
-*/
-    var startR = new moment("2019-01-15T13:00:00");
-    var endR = new moment("2019-01-25T19:00:00");
-
-    var interval = moment.range(startR, endR);
-    //console.log(user.allUserEvents)
-    //var x = subtractRanges(interval, user.allUserEvents)
-   
-    //console.log(x)
-    /*let day = moment("00:00:00", "HH:mm:ss").range("day");
-    let ranges = user.allUserEvents; //[moment("10:00:00", "HH:mm:ss").range("hour"), moment("16:00:00", "HH:mm:ss").range("hour")];
-    console.log(subtractRanges(day, ranges))*/
-  //  let day = moment("00:00:00", "HH:mm:ss").range("day");
-//let ranges = [moment("10:00:00", "HH:mm:ss").range("hour"), moment("16:00:00", "HH:mm:ss").range("hour")];
-//console.log(user.allUserEvents[0])
-events = []
-e = null;
-for (i=0; i<user.allUserEvents.length; i++) {
-    e = moment.range(user.allUserEvents[i].start, user.allUserEvents[i].end); 
-    events.push(e);
-}
-//console.log(events);
-console.log(subtractRanges(interval, events))
-//console.log(subtractRanges(interval, user.allUserEvents));
-
-
-//var answers = findGroupFreeTimes(user, "2019-01-05T13:00:00", "2019-01-30T19:00:00", false, 0);
-
-
-
-    //console.log(startR);
-    //console.log(endR);
-    //console.log(interval);
-    /*
-    var busyRange = null;
-    var startB = null;
-    var endB = null;
-    //var interva = null;
-    // subtracting unusual times 6-8pm
-
-    // subtracting busy times
-    intervalAux = [];
-    for (var i = 0; i < user.allUserEvents.length; i++)  {
-
-        // startB = new Date(user.allUserEvents[i].start);
-        // endB = new Date(user.allUserEvents[i].end);
-        var inter  =  user.allUserEvents[i].start.concat("/");
-        inter = inter.concat(user.allUserEvents[i].end);
-        var busyRange = moment.rangeFromISOString(inter);
-        //intervalAux.push(interval.subtract(busyRange));
-        interval = interval.subtract(busyRange)
-        console.log(interval)
-        }
-        console.log(interval) */
-
-
-    
-
-
-
-
-
-
-
-
-        // var rangeStart = new Date(user.allUserEvents[0].start);
-        // //console.log(rangeStart);
-        //  var rangeEnd = new Date(user.allUserEvents[0].end);
-        //  var busyRange = moment.range(rangeStart, rangeEnd);
-        //  //console.log(busyRange.toString())
-        //  interval = interval.subtract(busyRange)
-
-
-        //console.log("THE INTERVAL:")
-        //console.log(interval.length);
-
-    // console.log("interval")
-    //     console.log(interval)
-    //     console.log(interval.length)
-
-    /*
-    flatted the nested array.
-    */
-//    
-
-
-    /*
-        interval is free times between users. If hasDuration == true, return chunks of time greater 
-        than or equal to duration. Else return all chunks. 
-    */
-//     finalFreeTime = [];
-//     if (hasDuration){
-//         for (var x in interval) {
-//             if (x.diff('minutes') >= duration) {
-//                 finalFreeTime.append(x.format('LLL'));
-//             }
-//         }
-//     }
-//     else {
-//         finalFreeTime = interval;
-//     }
-//     console.log()
-//     //console.log(finalFreeTime);
-//     return finalFreeTime;
-//  }
-//for (var x of answers) {
-
-
-
-
-    // console.log(user.allUserEvents);
-    // console.log(user.allUserEvents.length)
-    // console.log(user.allUserEvents[0])
-    // //what I want below
-    // console.log(user.allUserEvents[0].start)
-
-//}
-=======
 
    
    
    
 
->>>>>>> ede40893633336c6ba3d10ef494afb0ab4f9ae63
 function flatten (input) {
     var flattened=[];
     for (var i=0; i<input.length; ++i) {
@@ -283,9 +156,6 @@ function test () {
 
 }
 
-<<<<<<< HEAD
-//test();
-=======
 /*
 duration should be sent in as minutes.
 */
@@ -294,4 +164,3 @@ var endR = new moment("2019-01-25T19:00:00");
 
 var longRanges = moment.range(startR, endR);
 subtractRanges(longRanges, user);
->>>>>>> ede40893633336c6ba3d10ef494afb0ab4f9ae63
